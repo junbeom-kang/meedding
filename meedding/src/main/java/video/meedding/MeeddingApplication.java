83151ba0ea76b9c937b2b52c97558120767b8bf7
junbeom-kang <kgood10369@gmail.com>
2021-05-06 23:53:17 +0900

    프로젝트 생성+ gradle 설정 +git 설정

---
+++ meedding/src/main/java/video/meedding/MeeddingApplication.java
@@ -0,0 +1,13 @@
+package video.meedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeddingApplication.class, args);
+	}
+
+}
---
+++ meedding/src/test/java/video/meedding/MeeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package video.meedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
