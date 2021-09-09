cc756cb9885e3fc479f14f6120104a6dcaa74c79
junbeom-kang <kgood10369@gmail.com>
2021-09-09 21:30:45 +0900

    의존성 추가

--- meedding/src/main/java/video/meedding/Meedding/service/RoomService.java
+++ meedding/src/main/java/video/meedding/Meedding/service/RoomService.java
@@ -2,6 +2,7 @@ package video.meedding.Meedding.service;
 
 import io.openvidu.java.client.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import video.meedding.Meedding.domain.Member;
