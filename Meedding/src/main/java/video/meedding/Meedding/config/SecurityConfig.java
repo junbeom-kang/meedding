09411db33a660d78c3418568f34319c53b69547c
junbeom-kang <kgood10369@gmail.com>
2021-09-22 16:47:54 +0900

    CORS 배포환경 변경

--- Meedding/src/main/java/video/meedding/Meedding/config/SecurityConfig.java
+++ Meedding/src/main/java/video/meedding/Meedding/config/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.addFilter(corsConfig.corsFilter())
-                //.cors().and()
+                .addFilter(corsConfig.corsFilter())
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
