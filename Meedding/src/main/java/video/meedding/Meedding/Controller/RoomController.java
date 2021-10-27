3bf3d148032926744f5ddd6dd773faabf6028198
junbeom-kang <kgood10369@gmail.com>
2021-10-27 23:52:26 +0900

    Swagger를 통한 API 정의 상세화

--- Meedding/src/main/java/video/meedding/Meedding/Controller/RoomController.java
+++ Meedding/src/main/java/video/meedding/Meedding/Controller/RoomController.java
@@ -2,6 +2,8 @@ package video.meedding.Meedding.Controller;
 
 import io.openvidu.java.client.OpenViduHttpException;
 import io.openvidu.java.client.OpenViduJavaClientException;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,19 +24,22 @@ import java.util.stream.Collectors;
 public class RoomController {
     private final RoomService roomService;
     private final ResponseService responseService;
-
+    @ApiOperation(value = "회의방 생성", notes = "회의방 생성 성공시 세션값을 반환한다.")
+    @ApiImplicitParam(name = "Authorization", value = "access-token", required = true, dataType = "String", paramType = "header")
     @PostMapping("/rooms")
     public Result createRoom(@RequestBody RoomDto roomDto, @AuthenticationPrincipal PrincipalDetails principal) throws OpenViduJavaClientException, OpenViduHttpException {
         String session = roomService.Room(principal.getMember().getId(), roomDto);
         return responseService.getSingleResult(session);
     }
-
+    @ApiOperation(value = "회의방 참가", notes = "회의방 참가 성공시 토큰값을 반환한다.")
+    @ApiImplicitParam(name = "Authorization", value = "access-token", required = true, dataType = "String", paramType = "header")
     @PostMapping("/rooms/join")
     public Result join(@RequestBody RoomDto roomDto, @AuthenticationPrincipal PrincipalDetails principal) throws OpenViduJavaClientException, OpenViduHttpException {
         String session = roomService.join(principal.getMember().getId(), roomDto);
         return responseService.getSingleResult(session);
     }
-
+    @ApiOperation(value = "회의방 목록", notes = "회의방 목록을 반환한다.")
+    @ApiImplicitParam(name = "Authorization", value = "access-token", required = true, dataType = "String", paramType = "header")
     @GetMapping("/rooms")
     public Result roomList() {
         List<Room> rooms = roomService.allRoom();
@@ -43,18 +48,21 @@ public class RoomController {
                 .collect(Collectors.toList());
         return responseService.getListResult(room);
     }
-
+    @ApiOperation(value = "회의방 제목으로 찾기", notes = "회의방을 제목으로 검색하여 반환한다.")
+    @ApiImplicitParam(name = "Authorization", value = "access-token", required = true, dataType = "String", paramType = "header")
     @PostMapping("/rooms/title")
     public Result findByTitle(@RequestBody RoomTitleRequestDto roomTitleRequestDto) {
         return responseService.getSingleResult(roomService.findByTitle(roomTitleRequestDto.getTitle()));
     }
-
+    @ApiOperation(value = "회의방 제거", notes = "회의방을 제거한다")
+    @ApiImplicitParam(name = "Authorization", value = "access-token", required = true, dataType = "String", paramType = "header")
     @DeleteMapping("/rooms/{room_id}")
     public Result deleteRoom(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long room_id) {
         roomService.deleteRoom(principalDetails.getMember().getId(), room_id);
         return responseService.getSuccessResult();
     }
-
+    @ApiOperation(value = "회의방 나가기", notes = "회의방 세션에서 나가지고 참여자를 줄인다")
+    @ApiImplicitParam(name = "Authorization", value = "access-token", required = true, dataType = "String", paramType = "header")
     @GetMapping("rooms/leave/{room_id}")
     public Result leaveRoom(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long room_id, @RequestHeader("token") String token) throws OpenViduJavaClientException, OpenViduHttpException {
         roomService.leaveSession(principalDetails.getMember().getId(), room_id, token);
@@ -65,7 +73,8 @@ public class RoomController {
     public void deleteAll() {
         roomService.deleteAll();
     }
-
+    @ApiOperation(value = "회의방 참여자 목록", notes = "해당 회의방의 참여자 목록을 반환한다.")
+    @ApiImplicitParam(name = "Authorization", value = "access-token", required = true, dataType = "String", paramType = "header")
     @GetMapping("rooms/People/{room_id}")
     public Result getParticipateList(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long room_id){
         List<ResponseParticipateDto> participate = roomService.getParticipate(room_id);
