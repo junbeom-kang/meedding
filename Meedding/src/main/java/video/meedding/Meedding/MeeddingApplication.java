d94114e5db9647de1cb8f8d34c956f3d34f4baf8
junbeom-kang <kgood10369@gmail.com>
2021-12-14 20:29:24 +0900

    배포 버전 확인 로그

--- Meedding/src/main/java/video/meedding/Meedding/MeeddingApplication.java
+++ Meedding/src/main/java/video/meedding/Meedding/MeeddingApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class MeeddingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("배포 버전확인 21-12-14");
 		SpringApplication.run(MeeddingApplication.class, args);
 	}
 
